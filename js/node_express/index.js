3d43b8588a5a93eb069bc8d267ff1c078eecc69e
kihyuns <qkdro78@naver.com>
2023-03-26 16:46:03 +0900

    파일정리

---
+++ js/node_express/index.js
@@ -0,0 +1,12 @@
+// //터미널에 npm i experss 명령어로 express 설치
+
+// const experss = require('experss')
+// const app = experss()
+
+// app.get('/', function (req,res) {
+//     res.send('Hello World')
+// })
+
+// app.listen(3000)
+
+console.log("Hellow")
